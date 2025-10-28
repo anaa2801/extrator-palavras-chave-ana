fd166f427cc30e597a53f038814a9a763e649adb
anaa2801 <alberton.ana@escola.pr.gov.br>
2025-10-28 10:45:07 -0300

    28/10

--- script.js
+++ script.js
@@ -13,7 +13,18 @@ function mostraPalavrasChaves(){
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
 
-    let frequencias = {};
+    const frequencias = contaFrequencias(palavras);
+let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+function ordenaPalavra(p1, p2){
+    return frequencias [p2] - frequencias[p1];
+}
+  return ordenadas.slice(0,10);
+}
+
+function contaFrequencias(palavras){
+
+  let frequencias = {};
 
     for(let i of palavras){
         frequencias[i]=0;
@@ -26,4 +37,7 @@ function processaTexto(texto){
     }
 
     return palavras;
+
+
+
 }
