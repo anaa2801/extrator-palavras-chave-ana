c65221e6b1995b1367c3ebfd4faf6be00dfa4dbf
anaa2801 <alberton.ana@escola.pr.gov.br>
2025-11-04 09:38:33 -0300

    04/11

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ function mostraPalavrasChaves(){
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
 
+    for (let i in palavras){
+        palavras[i]=palavras[i].toLowerCase();
+    }
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
 let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
@@ -37,7 +42,5 @@ function contaFrequencias(palavras){
     }
 
     return palavras;
-
-
 
 }
